47ff382f8238f8d19b7e0c1825f63041c88704dd
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2017-12-22 14:18:23 +0200

    lesson 22

--- src/Service/Catalogue.php
+++ src/Service/Catalogue.php
@@ -28,4 +28,14 @@ class Catalogue
         return $repo->findAll();
     }
 
+    /**
+     * @return Category[]|array
+     */
+    public function getTopCategories()
+    {
+        $repo = $this->em->getRepository(Category::class);
+
+        return $repo->findBy(['parent' => null], ['name' => 'ASC']);
+    }
+
 }
