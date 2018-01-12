c400335667ba554bf350ce6c7a6b98356946ff7e
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2018-01-12 20:24:03 +0200

    lesson 26. Forms завершение формы обратной связи

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Feedback;
 use App\Form\FeedbackType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -43,9 +45,32 @@ class AboutController extends Controller
      *
      * @param Request $request
      */
-    public function feedback(Request $request)
+    public function feedback(Request $request, EntityManagerInterface $em, \Swift_Mailer $mailer)
     {
-        $form = $this->createForm(FeedbackType::class);
+        $feedback = new Feedback();
+        $form = $this->createForm(FeedbackType::class, $feedback);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $em->persist($feedback);
+            $em->flush();
+            $this->addFlash('info', 'Спасибо за обращение!');
+
+            $message = (new \Swift_Message('Обращение с сайта'))
+                ->setFrom([getenv('MAILER_FROM') => getenv('MAILER_FROM_NAME')])
+                ->setTo(getenv('ADMIN_EMAIL'))
+                ->setBody(
+                    $this->renderView(
+                        'about/message.html.twig',
+                        array('feedback' => $feedback)
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            return $this->redirectToRoute('feedback');
+        }
 
         return $this->render('about/feedback.html.twig', [
             'form' => $form->createView(),
